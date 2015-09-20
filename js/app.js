809531a7264be3abe7c3364ef99fd20126dffa46
jlybianto <jesse.lybianto@gmail.com>
2015-09-20 17:35:25 -0600

    Adding Hadouken Sound

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 	});
 	$('.ryu').mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
@@ -28,3 +29,9 @@ $(document).ready(function() {
 		// Ryu goes back to ready position
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
