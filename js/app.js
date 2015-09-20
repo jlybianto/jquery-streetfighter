fded178cb3b7e40219c2d68bf2755654a42222c1
jlybianto <jesse.lybianto@gmail.com>
2015-09-20 17:21:59 -0600

    Removing Console Log

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 	});
 	$('.ryu').mousedown(function() {
-		console.log('mousedown');
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show();
@@ -16,7 +15,6 @@ $(document).ready(function() {
 		// show Hadouken animation to the right
 	});
 	$('.ryu').mouseup(function() {
-		console.log('mouseup');
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
 		// Ryu goes back to ready position
